ead81e1047fd9b3bb499b34e30a7ecb16cb3cf61
paulokruz <paulokruz@hotmail.com>
2022-02-02 13:06:36 +0000

    Adicionamos o componente Alert

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,33 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor(public alertController: AlertController) { }
+
+  async exibirAlertFavorito() {
+    const alert = await this.alertController.create({
+      /* cssClass: 'my-custom-class', */
+      header: 'Alerta!',
+      message: 'Adicionar aos <strong>favoritos</strong>!!!',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          /* cssClass: 'secondary', */
+          id: 'cancel-button',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Confirmar',
+          id: 'confirm-button',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 
 }
