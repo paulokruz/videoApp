f8ea3a24f4e63a60c07d6f9beb2cb6aa5f458bbb
paulokruz <paulokruz@hotmail.com>
2022-02-02 14:46:18 +0000

    Adicionado array e interface

---
+++ src/app/models/IFilme.model.ts
@@ -0,0 +1,7 @@
+export interface IFilme {
+  nome: string;
+  descricao: string;
+  genero: string[];
+  cartaz: string;
+  classificacao: number;
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { IFilme } from '../models/IFilme.model';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
@@ -8,6 +9,31 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+
+  titulo = 'Vídeos App';
+  listaVideos: IFilme[] = [
+    {
+      nome: 'Mother/Android (2021)',
+      descricao: 'Georgia and her boyfriend Sam go on a treacherous journey to escape their country.',
+      genero: ['Science', 'Fiction', 'Thriller'],
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/rO3nV9d1wzHEWsC7xgwxotjZQpM.jpg',
+      classificacao: 58
+    },
+    {
+      nome: 'Riverdance: The Animated Adventure (2021)',
+      descricao: 'A young Irish boy named Keegan and Spanish girl named Moya journey into a magical world of the Megaloceros Giganteus.',
+      genero: ['Animation', 'Fantasy', 'Music', 'Adventure', 'Comedy'],
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/i6TRDlVuuEXMyLSlIkDd8YhBN0r.jpg',
+      classificacao: 56
+    },
+    {
+      nome: 'The Wheel of Time (2021)',
+      descricao: 'Follow Moiraine, a member of the shadowy and influential all-female organization called the “Aes Sedai”.',
+      genero: ['Sci-Fi', 'Fantasy', 'Drama'],
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/mpgDeLhl8HbhI03XLB7iKO6M6JE.jpg',
+      classificacao: 79
+    }
+  ];
 
   constructor(public alertController: AlertController, public toastController: ToastController) { }
 
